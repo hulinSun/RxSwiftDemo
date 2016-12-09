6dbc7e007ffcb06e0515a1b950249f654b3ea0b9
Hony <438661443@qq.com>
2016-12-09 18:27:52 +0800

    f

--- RxSwiftDemo/RxSwiftDemo/TransformViewController.swift
+++ RxSwiftDemo/RxSwiftDemo/TransformViewController.swift
@@ -14,6 +14,8 @@ import RxBlocking
 
 class TransformViewController: UIViewController {
 
+    let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -24,7 +26,8 @@ class TransformViewController: UIViewController {
         super.touchesBegan(touches, with: event)
 //        map()
 //        mapWithIdx()
-        flatmap()
+//        flatmap()
+        scan()
     }
     
     
@@ -50,7 +53,6 @@ class TransformViewController: UIViewController {
     func flatmap()  {
         
         //FIXME: 这里有问题吧?
-        let bag = DisposeBag()
         let obr1 = Observable.of(1,2,3)
         let obr2 = Observable.of("A","B","C","D");
 
@@ -58,6 +60,22 @@ class TransformViewController: UIViewController {
             print("----- \(num) ----")
             return obr2
         }.subscribe { (e) in
+            print(e)
+        }.addDisposableTo(bag)
+        
+        //flatlatest:暂时归在 flatMap 中，和 flatMap 不同的就是，当进行 flatMap 转换时有新的值发射过来时就丢弃旧的值，去 flatMap 新的值。
+            
+        // flatfitst:flatMapFirst 和 flatMapLatest 不同就是 flatMapFisrt 会选择旧的值，抛弃新的。
+    }
+    
+    func scan() {
+        
+        // scan:应用一个 accumulator (累加) 的方法遍历一个序列，然后返回累加的结果。此外我们还需要一个初始的累加值。实时上这个操作就类似于 Swift 中的 reduce 。
+        // 那些返回的结果各个，并不相加 之后返回，只是返回当前这一步骤计算的结果
+        Observable.of(1,2,3,4).scan(3, accumulator: { acum ,elem in
+            print("acum = \(acum) , elem = \(elem)")
+            return acum * elem
+        }).subscribe { (e) in
             print(e)
         }.addDisposableTo(bag)
     }
